ee60cc46c086d0ec353219b6c9c161c11d20426a
admin-PC\Administrator <devilsuraj@gmail.com>
2017-02-25 16:05:55 +0530

    cvcv

--- ArtfriksAngular/src/app/app.module.ts
+++ ArtfriksAngular/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {SharedModule} from './shared/sharedmodule'
-import {MODULE_ROUTES} './shared/routes/routes';
+import {MODULE_ROUTES} from './shared/routes/routes';
 import { HttpModule} from '@angular/http';
 import { FormsModule }    from '@angular/forms';
 import { AppComponent }   from './app.component';
